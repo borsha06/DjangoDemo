b51fd269039ae80ef54f0cf8ff0f974f4f480a43
rofi93 <khairul.basar93@gmail.com>
2018-09-17 20:55:22 +0600

    jquery fixed

--- static/assets/js/address.js
+++ static/assets/js/address.js
@@ -1,18 +1,15 @@
 
-$('#district').hide();
-$('#thana').hide();
-$('#postal').hide();
 $(document).ready(function() {
     $.ajax({
         url: 'http://45.33.76.177:8500/address/divisions/',
         type: 'get',
         success:function(data){
-            //var result = $('select').find('option');
-            console.log(data);
+
             var options ="";
             for (var i = 0; i < data.length; i++) {
-              options += "<option value="+data[i]+">" + data[i] + "</option>";
+              options += "<option value=" + data[i] + ">" + data[i] + "</option>";
             }
+
             $("#division").append(options);
 
          }
@@ -21,101 +18,90 @@ $(document).ready(function() {
 
     $('#division').on('change', function(){
         var division = $(this).val();
-        console.log(division);
+
         if(division !== "0" ){
 
             $.ajax({
-            url: 'http://45.33.76.177:8500/address/districts/',
-            type: 'get',
-            data:{
-                'division':division
-            },
-            success:function(data){
-                //var result = $('select').find('option');
-                console.log(data);
-                var options ="";
-                 $('select #district').children('option:not(:first)').remove();
-                 for (var i = 0; i < data.length; i++) {
-                 options += "<option value="+data[i]+">" + data[i] + "</option>";
-            }
-            $("#district").append(options);
+                url: 'http://45.33.76.177:8500/address/districts/',
+                type: 'get',
+                data:{
+                    'division':division
+                },
+                success:function(data){
+
+                    var options ="<option value='0'>-------</option>";
+
+                    $('#district option').each(function(){
+                        ($this).remove();
+                    });
+
+                    for (var i = 0; i < data.length; i++) {
+                        options += "<option value="+data[i]+">" + data[i] + "</option>";
+                    }
+                    $("#district").append(options);
+                }
+            });
         }
-
-
     });
-      $('#district').show();
 
     $('#district').on('change', function(){
         var district = $(this).val();
 
         if(district !== "0" )
         {
-        $.ajax({
-        url: 'http://45.33.76.177:8500/address/thanas/',
-        type: 'get',
-        data:{
-        'division':division,
-        'district':district
-        },
-        success:function(data){
-        //var result = $('select').find('option');
-        console.log(data);
-        var options ="";
+            $.ajax({
+                url: 'http://45.33.76.177:8500/address/thanas/',
+                type: 'get',
+                data:{
+                    'division':division,
+                    'district':district
+                },
+                success:function(data){
 
-        for (var i = 0; i < data.length; i++) {
-      $('select #thana').children('option:not(:first)').remove();
-        options += "<option value="+data[i]+">" + data[i] + "</option>";
+                    var options ="<option value='0'>-------</option>";
+
+                    $('#district option').each(function(){
+                        ($this).remove();
+                    });
+
+                    for (var i = 0; i < data.length; i++) {
+                        options += "<option value=" + data[i] + ">" + data[i] + "</option>";
+                    }
+                    $("#thana").append(options);
+                }
+            });
         }
-        $("#thana").append(options);
-
-        }
-
-    });
-    $('#thana').show();
-        console.log(district);
-
-        }
-
   });
-    $('#thana').on('change', function(){
+
+$('#thana').on('change', function(){
 
     var thana = $(this).val();
     var district = $('#district').val();
 
-     if(thana !== "0" )
-        {
-            $.ajax({
-                url: 'http://45.33.76.177:8500/address/postal-code/',
-                type: 'get',
-                data:{
+    if(thana !== "0" )
+    {
+        $.ajax({
+            url: 'http://45.33.76.177:8500/address/postal-code/',
+            type: 'get',
+            data:{
                 'district':district,
                 'thana':thana,
-                },
-                success:function(data){
-                    //var result = $('select').find('option');
-                     console.log(data);
-        var options ="";
+            },
+            success:function(data){
 
-        for (var i = 0; i < data.length; i++) {
-        $('select #postal').children('option:not(:first)').remove();
-        options += "<option value="+data[i]+">" + data[i] + "</option>";
-        }
-        $("#postal").append(options);
-                    console.log(data[1]);
+                var options ="<option value='0'>-------</option>";
+
+                $('#thana option').each(function(){
+                    ($this).remove();
+                });
+
+                for (var i = 0; i < data.length; i++) {
+                    options += "<option value="+data[i]+">" + data[i] + "</option>";
                 }
-            });
-             $('#postal').show();
-
-       }
-  });
-   }
-   else{
-   $('#district').hide();}
-
-  });
-
-
-
-
-
+                $("#postal").append(options);
+            }
+        });
+    }
 });
+
+  });
